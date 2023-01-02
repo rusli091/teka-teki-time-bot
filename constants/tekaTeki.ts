903ac8c0e8214ee9ee9f1d7cd5fda88014f9e504
Tengku Hafidz <tengkuhafidz@gmail.com>
2023-01-02 20:14:46 +0800

    add teka teki

--- constants/tekaTeki.ts
+++ constants/tekaTeki.ts
@@ -4,5 +4,17 @@ export const tekaTekis: TekaTeki[] = [
     {
         question: "Banyak-banyak buah, buah apa yang bile orang makan, die akan jadi kurus? 🍓🥑🍇",
         answer: "Man-goes-thin! 😜"
+    },
+    {
+        question: "Dalam banyak-banyak beg, beg apa yang boleh menyanyi? 🎤🎶",
+        answer: "Begstreet Boys 🎒👨🏼‍🎤"
+    },
+    {
+        question: "Dalam banyak banyak kopi, kopi apa yang masam? ☕️",
+        answer: "Kopikirlasendiri 🙊"
+    },
+    {
+        question: "Dalam banyak-banyak kotak, kotak apa yang berdosa? 📦",
+        answer: "Kotaksembahyang 😈🧎🏼‍♂️"
     }
 ]
